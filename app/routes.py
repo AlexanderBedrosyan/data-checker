0ea4b7ba2978d481818e290a4194fd41f16bd2ac
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-05-16 01:29:44 +0300

    Update routes

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,11 @@
 # app/routes.py
-from flask import Blueprint, render_template
-import logging
-logging.basicConfig(level=logging.DEBUG)
+from flask import Blueprint, render_template, current_app
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/', strict_slashes=False)
 def home():
-    app.logger.info("Home route hit")
+    current_app.logger.info("Home route hit")
     return render_template('index.html')
-
 
