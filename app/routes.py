5b9d500d159461f18a66f01df89fff216aafa50a
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-05-15 09:47:55 +0300

    Add routes

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,12 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
+def home():
+    return render_template('index.html')
+
+
+@main.route('')
 def home():
     return render_template('index.html')
