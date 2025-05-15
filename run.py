8a2a3cdb8fa015c84c36a0993ab3fce0857830db
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-05-15 16:44:27 +0300

    Update files

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 3000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
