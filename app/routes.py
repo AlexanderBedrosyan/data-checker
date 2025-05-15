10ef7f3433b1ddf29c0d25518f4de7a2f9deeb5e
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-05-15 16:54:52 +0300

    Updates

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 # app/routes.py
 from flask import Blueprint, render_template
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/', strict_slashes=False)
 def home():
+    app.logger.info("Home route hit")
     return render_template('index.html')
 
 
