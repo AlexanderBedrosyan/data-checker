60353790a188bd56d2975055336a1e3fd98c8d7a
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-05-20 16:54:07 +0300

    Create model and add route

--- app/models.py
+++ app/models.py
@@ -1,17 +1,29 @@
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+# from flask_sqlalchemy import SQLAlchemy
+# from werkzeug.security import generate_password_hash, check_password_hash
+#
+# db = SQLAlchemy()
+#
+#
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password_hash = db.Column(db.String(128), nullable=False)
+#
+#     def set_password(self, password):
+#         self.password_hash = generate_password_hash(password)
+#
+#     def check_password(self, password):
+#         return check_password_hash(self.password_hash, password)
 
-db = SQLAlchemy()
+
+from flask_login import UserMixin
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+class EnvUser(UserMixin):
+    def __init__(self, email):
+        self.id = 1  # fixed ID since we only have one user
+        self.email = email
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    def get_id(self):
+        return str(self.id)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
-from .views import HomeView, LoginView
+from .views import HomeView, LoginView, LogoutView
 from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
 
+auth = Blueprint('auth', __name__)
 main = Blueprint('main', __name__)
 
 main.add_url_rule('/', view_func=HomeView.as_view('home'))
 main.add_url_rule('/login', view_func=LoginView.as_view('login'))
+main.add_url_rule('/logout', view_func=LogoutView.as_view('logout'))
 
