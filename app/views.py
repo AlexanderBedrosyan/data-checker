125ca96c565a1d9e65494ad098fc3f0aa29bdf09
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-05-20 16:54:54 +0300

    Update index as add credentials for login and create 3 views for home, login, logout

--- app/views.py
+++ app/views.py
@@ -2,7 +2,12 @@ from flask.views import MethodView
 from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash
-from app.models import User
+from app.models import EnvUser
+import os
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
 
 auth_blueprint = Blueprint('auth', __name__)
 
@@ -18,11 +23,15 @@ class LoginView(MethodView):
         email = request.form.get('email')
         password = request.form.get('password')
 
-        user = User.query.filter_by(username=username, email=email).first()
+        private_username = os.getenv("USER")
+        private_password = os.getenv("PASSWORD")
+        private_email = os.getenv("EMAIL")
 
-        if user and check_password_hash(user.password, password):
+        if username == private_username and email == private_email and password == private_password:
+            user = EnvUser(email)
             login_user(user)
             return redirect(url_for('main.home'))
+
         else:
             flash('Invalid login credentials.', 'danger')
             return render_template('index.html')
@@ -33,5 +42,13 @@ class HomeView(MethodView):
 
     def get(self):
         current_app.logger.info("Home route hit")
-        return render_template('index.html')
+        return render_template('home.html')
 
+
+class LogoutView(MethodView):
+    decorators = [login_required]
+
+    def get(self):
+        logout_user()
+        flash("Logged out successfully.", "success")
+        return redirect(url_for('main.login'))
