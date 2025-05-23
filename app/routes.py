d8493872e31b09e2ceb120a5765f476b7e19aba6
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-05-23 15:12:06 +0300

    Add new routes for upload and analyze cbv

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from .views import HomeView, LoginView, LogoutView
+from .views import HomeView, LoginView, LogoutView, UploadView, AnalyzeView
 from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
 
 auth = Blueprint('auth', __name__)
@@ -7,4 +7,6 @@ main = Blueprint('main', __name__)
 main.add_url_rule('/', view_func=HomeView.as_view('home'))
 main.add_url_rule('/login', view_func=LoginView.as_view('login'))
 main.add_url_rule('/logout', view_func=LogoutView.as_view('logout'))
+main.add_url_rule('/upload', view_func=UploadView.as_view('upload'))
+main.add_url_rule('/analyze', view_func=AnalyzeView.as_view('analyze'))
 
