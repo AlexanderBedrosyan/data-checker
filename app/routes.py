9a43f8ca61611478bfd2963d6f1f4d6fc25895fa
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-06-03 18:51:11 +0300

    Change routes

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,25 @@
 from .views import HomeView, LoginView, LogoutView, UploadView, AnalyzeView
-from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app
+from flask import Blueprint, render_template, request, redirect, url_for, flash, current_app, jsonify
 
 auth = Blueprint('auth', __name__)
 main = Blueprint('main', __name__)
+
+
+@main.route('/get-file-options')
+def get_file_options():
+    query = request.args.get('q', '').lower()
+
+    files = [
+        {'id': '1', 'text': 'Gosho'},
+        {'id': '2', 'text': 'Ivan'},
+        {'id': '3', 'text': 'Dragan'},
+        {'id': '3', 'text': 'Petkan'},
+    ]
+
+    filtered = [f for f in files if query in f['text'].lower()]
+
+    return jsonify(filtered)
+
 
 main.add_url_rule('/', view_func=HomeView.as_view('home'))
 main.add_url_rule('/login', view_func=LoginView.as_view('login'))
