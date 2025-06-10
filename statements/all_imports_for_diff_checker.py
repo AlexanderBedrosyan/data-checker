6aa95a483d38bb913e627dcb0ae34caa5e8aaec4
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2025-06-10 13:45:12 +0300

    update all imports

--- statements/all_imports_for_diff_checker.py
+++ statements/all_imports_for_diff_checker.py
@@ -1,41 +1,41 @@
 from statements.bc_balances.bc_balance import *
 from statements.ah_fragt.ah_fragt_vendor_balance import *
-from statements.ntg_dk_neptun.neptun_vendor_balance import *
-from statements.freja_transport.freja_vendor_balance import *
-from statements.sft_logistics_dk.sft_logistics_vendor_balance import *
-from statements.timmerman.timmerman_vendor_balance import *
-from statements.dfds_seaways_ab.dfds_seaways_ab_vendor_balance import *
-from statements.vendelbo.vendelbo_vendor_balance import *
-from statements.nordjysk_trailer_udlejning.nordjysk_trailer_udlejning_vendor_balance import *
-from statements.fjord_line_as.fjord_line_as_vendor_balance import *
-from statements.samarina_uab.samarina_uab_vendor_balance import *
-from statements.europart_dk.europart_dk_vendor_balance import *
-from statements.kh_onestop.kh_onestop_vendor_balance import *
-from statements.besko_as.besko_as_vendor_balance import *
-from statements.stabrand_spedition.stabrand_spedition_vendor_balance import *
-from statements.thurah_transport_as.thurah_transport_as_vendor_balance import *
-from statements.brink_transport_aps.brink_traponsport_vendor_balance import *
-from statements.scan_shipping_as.scan_shipping_as_vendor_balance import *
-from statements.cargo_uab.cargo_uab_vendor_balance import *
-from statements.norfragt_aps.norfragt_aps_vendor_balance import *
-from statements.daarbak.daarbak_vendor_balance import *
-from statements.j_lauritzens.j_lauritzens_vendor_balance import *
-from statements.east_transportation_ltd.east_transportation_ltd_vendor_balance import *
-from statements.therkelsen_h_p_as.therkelsen_vendor_balance import *
-from statements.jensen_k_e_aps.jensen_aps_vendor_balance import *
-from statements.blue_water.blue_water_vendor_balance import *
-from statements.delamode_baltics_uab.delamode_vendor_balance import *
-from statements.nielsen_and_sorensen.nielsen_and_sorensen_vendor_balance import *
-from statements.schenker_as.schenker_vendor_balance import *
-from statements.nc_scandinavia_as.nc_scandinavia_as_vendor_balance import *
-from statements.fairtrans_dk.fairtrans_dk_vendor_balance import *
-from statements.dfds_as.dfds_as_vendor_balance import *
-from statements.transfennica_ltd.transfennica_ltd_vendor_balance import *
-from statements.tt_line.tt_line_vendor_balance import *
-from statements.stena_line_scandinavia_ab.stena_line_scandinavia_vendor_balance import *
-from statements.dsv_road_as.dsv_road_vendor_balance import *
-from statements.hcs_transort_spedition.hcs_transport_spedition import *
-from statements.svend_e_sorensen.svend_e_sorensen_vendor_balance import *
-from statements.egsj.egsj_vendor_balance import *
-from statements.dtk_road.dtk_road_vendor_balance import *
-from statements.jan_petersen.jan_petersen import *
+# from statements.ntg_dk_neptun.neptun_vendor_balance import *
+# from statements.freja_transport.freja_vendor_balance import *
+# from statements.sft_logistics_dk.sft_logistics_vendor_balance import *
+# from statements.timmerman.timmerman_vendor_balance import *
+# from statements.dfds_seaways_ab.dfds_seaways_ab_vendor_balance import *
+# from statements.vendelbo.vendelbo_vendor_balance import *
+# from statements.nordjysk_trailer_udlejning.nordjysk_trailer_udlejning_vendor_balance import *
+# from statements.fjord_line_as.fjord_line_as_vendor_balance import *
+# from statements.samarina_uab.samarina_uab_vendor_balance import *
+# from statements.europart_dk.europart_dk_vendor_balance import *
+# from statements.kh_onestop.kh_onestop_vendor_balance import *
+# from statements.besko_as.besko_as_vendor_balance import *
+# from statements.stabrand_spedition.stabrand_spedition_vendor_balance import *
+# from statements.thurah_transport_as.thurah_transport_as_vendor_balance import *
+# from statements.brink_transport_aps.brink_traponsport_vendor_balance import *
+# from statements.scan_shipping_as.scan_shipping_as_vendor_balance import *
+# from statements.cargo_uab.cargo_uab_vendor_balance import *
+# from statements.norfragt_aps.norfragt_aps_vendor_balance import *
+# from statements.daarbak.daarbak_vendor_balance import *
+# from statements.j_lauritzens.j_lauritzens_vendor_balance import *
+# from statements.east_transportation_ltd.east_transportation_ltd_vendor_balance import *
+# from statements.therkelsen_h_p_as.therkelsen_vendor_balance import *
+# from statements.jensen_k_e_aps.jensen_aps_vendor_balance import *
+# from statements.blue_water.blue_water_vendor_balance import *
+# from statements.delamode_baltics_uab.delamode_vendor_balance import *
+# from statements.nielsen_and_sorensen.nielsen_and_sorensen_vendor_balance import *
+# from statements.schenker_as.schenker_vendor_balance import *
+# from statements.nc_scandinavia_as.nc_scandinavia_as_vendor_balance import *
+# from statements.fairtrans_dk.fairtrans_dk_vendor_balance import *
+# from statements.dfds_as.dfds_as_vendor_balance import *
+# from statements.transfennica_ltd.transfennica_ltd_vendor_balance import *
+# from statements.tt_line.tt_line_vendor_balance import *
+# from statements.stena_line_scandinavia_ab.stena_line_scandinavia_vendor_balance import *
+# from statements.dsv_road_as.dsv_road_vendor_balance import *
+# from statements.hcs_transort_spedition.hcs_transport_spedition import *
+# from statements.svend_e_sorensen.svend_e_sorensen_vendor_balance import *
+# from statements.egsj.egsj_vendor_balance import *
+# from statements.dtk_road.dtk_road_vendor_balance import *
+# from statements.jan_petersen.jan_petersen import *
